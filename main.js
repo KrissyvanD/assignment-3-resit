d40e573a17a29efcc9ff3c2297bd3e604c598d6b
KrissyvanD <58208561+KrissyvanD@users.noreply.github.com>
2020-07-10 17:47:32 +0200

    commit x

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-
+const api = {
+key: "acdd0e3fbba77ba724190f904b36165a", 
+base: "https://api.openweathermap.org/data/2.5/"
+}
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jpc3N5dmFuZCIsImEiOiJja2Jxb3k3dHMyb2doMnJsOTUxNWw1cXMzIn0.j8ZqrOu1apV44ewk8V67bg';
 
@@ -31,9 +34,54 @@ var geojson = {
     },
     properties: {
       title: 'Mapbox',
-      description: 'San Francisco, California'
+      description: 'San Francisco, California',
+      }
+    },
+	{
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-115.139, 36.169]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'Las Vegas, Nevada '
     }
-  }]
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-87.623, 41.881]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'Chicago, Illinois '
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-73.935, 40.730]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'New York, New York '
+    }
+  },
+  {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [-95.358, 29.749]
+    },
+    properties: {
+      title: 'Mapbox',
+      description: 'Houston, Texas '
+    }
+  },
+  ]
 };
 
 // add markers to map
@@ -44,12 +92,24 @@ geojson.features.forEach(function(marker) {
   el.className = 'marker';
 
   // make a marker for each feature and add to the map
-  new mapboxgl.Marker(el)
+
+function callback(weather){
+	 mapMarker = new mapboxgl.Marker(el)
     .setLngLat(marker.geometry.coordinates)
     .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
-    .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+    	.setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>' + `${Math.round(weather.main.temp)}<span>°C</span>`)
+    	)
     .addTo(map);
+}
+getResults(marker.geometry.coordinates[0], marker.geometry.coordinates[1],callback);
+
 });
+
+function getResults(lon,lat, callback){
+	fetch(`${api.base}weather?lat=${lat}&lon=${lon}&units=metric&appid=${api.key}`)
+	.then(weather => {return weather.json()})
+	.then(callback)
+		}
 
 
 
